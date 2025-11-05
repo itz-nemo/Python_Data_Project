d1cd0344668f55cddb33454911f8fe24f97e3d7a
Nimish Tiwari <ajaynimishr3p@gmail.com>
2025-11-05 15:49:02 +0530

    new

--- 1_session.py
+++ 1_session.py
@@ -1,8 +1,23 @@
-print('hello world')
-day_1 = 200
-day_2 = 220
-day_3 = 185
-day_4 = 250
-total_sales = day_1 + day_2 + day_3 + day_4
-print('Total sales for the week:', total_sales)
-average_sales = total_sales / 4
+category_a = [200, 220, 185, 250]
+category_b = [150, 160, 170, 180]
+category_c = [300, 310, 320, 330]
+
+list_2 = [category_a,category_b,category_c]
+for i in list_2:
+    sum = 0
+    for j in i:
+        sum += j
+    print(f'Sum of {i}={sum}') 
+
+
+# Using dictionary 
+categories = {
+    "category_a": [200, 220, 185, 250],
+    "category_b": [150, 160, 170, 180],
+    "category_c": [300, 310, 320, 330]
+}
+
+for name, values in categories.items():
+    total = sum(values)
+    print(f'sum of {name} is {total}')
+
